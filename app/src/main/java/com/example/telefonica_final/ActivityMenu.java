c3ca2fcd5e3be4fa8559c2b6232ed9dcdd238094
pipre94 <pipre94@gmail.com>
2017-11-23 07:49:39 -0500

    data

--- app/src/main/java/com/example/telefonica_final/ActivityMenu.java
+++ app/src/main/java/com/example/telefonica_final/ActivityMenu.java
@@ -3,14 +3,29 @@ package com.example.telefonica_final;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.telefonica_final.Data.DataTelefonica;
+import com.example.telefonica_final.Models.Pago;
+
 public class ActivityMenu extends AppCompatActivity {
+
+    DataTelefonica dataTelefonica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main);
+
+        dataTelefonica = new DataTelefonica(this);
+        dataTelefonica.open();
+
+        if (dataTelefonica.findAll().size()==0){
+            Log.i("LOGTAG", "Se creo una Deuda");
+            createData();
+        }
+        Log.i("LOGTAG", "Se creo una Deuda: "+ dataTelefonica.findAll().get(0).getDeuda());
     }
 
     public void exit(View view){
@@ -26,6 +41,13 @@ public class ActivityMenu extends AppCompatActivity {
     public void saldo(View view){
         Intent intent = new Intent(this, Mainsaldo.class);
         startActivity(intent);
+    }
+
+    private void createData(){
+        Pago pago = new Pago();
+        pago.setDeuda(300000);
+
+        dataTelefonica.createPago(pago);
     }
 
 
